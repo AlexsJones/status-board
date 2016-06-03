58218a6265cadd0abda0730860ee434ee41a5816
AlexsJones <tibbar@freedommail.ch>
2016-06-03 13:32:26 +0100

    added graphing

--- run.py
+++ run.py
@@ -1,20 +1,27 @@
 import json
 import subprocess
 from time import time
-
+import pygal
 import requests
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
 with open('config.json') as f:
     config = json.loads(f.read())
 
+class MyServer(Flask):
+    def __init__(self, *args, **kwargs):
+        super(MyServer,self).__init__(*args, **kwargs)
+        self.stored_data = {}
+        
+        for task in config['tasks']:
+            self.stored_data[task["id"]]=[]
+
+app = MyServer(__name__)
 
 def shell_handler(command, **kwargs):
     return_code = subprocess.call(command, shell=True)
-    return json.dumps(return_code == 0)
-
+    return json.dumps(return_code == 0),0
 
 def http_handler(address, **kwargs):
     s = time()
@@ -28,18 +35,22 @@ def http_handler(address, **kwargs):
 
 
 get_handler = {
-    'http': http_handler,
-    'shell': shell_handler,
-}
-
+        'http': http_handler,
+        'shell': shell_handler,
+        }
 
 @app.route('/')
 def index():
-    data = {
-        'tasks': config['tasks'],
-        'title': config['title'],
-    }
-    return render_template('index.html', **data)
+
+    title = "Title"
+    chart = pygal.Line(stroke=True,
+            width=1200,height=600,explicit_size=True,title=title)
+
+    for key, value in app.stored_data.iteritems():
+        chart.add(key,[float(i) for i in value])
+    
+    return render_template('index.html', tasks=config['tasks'],
+            title=config['title'],bar_chart=chart)
 
 
 @app.route('/<task_id>')
@@ -48,9 +59,16 @@ def status(task_id):
         task = next(task for task in config['tasks'] if task['id'] == task_id)
     except StopIteration:
         return 'This task does not exist', 404
-    j, t = get_handler[task['type']](**task)
+    
+    print("Next task to process is %s of type %s" % (task['id'],task['type']))
+
+    j,t = get_handler[task['type']](**task)
+
+    if float(t) < float(3000):
+        app.stored_data[task['id']].append(t)
+    else:
+        print("Omitting %s response time too high for graphing %s" % (task['id'],t))
     return json.dumps({"status": j, "time": "%sms" % str(t)})
 
-
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', threaded=True)
+ 
+app.run(debug=False, host='0.0.0.0', threaded=True)
